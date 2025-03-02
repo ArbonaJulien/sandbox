41b520adc8e891fc10c75c2b3509e7b9fa93bfab
ARBONA Julien <36328806+ArbonaJulien@users.noreply.github.com>
2025-03-02 16:34:40 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 (function () {
   // Your code here
-  var tl = gsap.timeline({
+  var tl_1 = gsap.timeline({
     scrollTrigger: {
       trigger: '.step-1',
       pin: true,
@@ -10,11 +10,33 @@
     },
   });
 
-  tl.from('.step-1 .copy-container', {
-    duration: 1,
-    opacity: 0,
-  }).from('.step-1 .copy', {
-    duration: 1,
-    opacity: 0,
+  tl_1
+    .from('.step-1 .copy-container', {
+      duration: 1,
+      opacity: 0,
+    })
+    .from('.step-1 .copy', {
+      duration: 1,
+      opacity: 0,
+    });
+
+  var tl_2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.step-2',
+      pin: true,
+      start: 'top 48px',
+      end: '+=1200',
+      scrub: 1,
+    },
   });
+
+  tl_2
+    .from('.step-2 .copy-container', {
+      duration: 1,
+      opacity: 0,
+    })
+    .from('.step-2 .copy', {
+      duration: 1,
+      y: '80vh',
+    });
 })();
