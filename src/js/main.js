8f14d330e5cb628137d193302ec5cbacc7250cdb
ARBONA Julien <36328806+ArbonaJulien@users.noreply.github.com>
2025-03-02 19:16:06 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -25,7 +25,7 @@
       trigger: '.step-2',
       pin: true,
       start: 'top 48px',
-      end: '+=1200',
+      end: '+=1600',
       scrub: 1,
     },
   });
@@ -37,6 +37,48 @@
     })
     .from('.step-2 .copy', {
       duration: 1,
-      y: '80vh',
+      y: '120vh',
+      ease: Linear,
     });
+
+  var tl_3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.step-3',
+      pin: true,
+      start: 'top 48px',
+      end: '+=500',
+      scrub: 1,
+      pinSpacing: false,
+    },
+  });
+
+  tl_3
+    .from(
+      '.step-3 .copy-container',
+      {
+        duration: 1,
+        backgroundColor: 'rgba(0,0,0, .7)',
+      },
+      '<'
+    )
+    .from(
+      '.step-3 .copy',
+      {
+        duration: 1,
+        opacity: 1,
+      },
+      '<'
+    )
+    .from('.step-3 .picture-container', {
+      duration: 1,
+      height: '100vh',
+    })
+    .to(
+      '.step-3.after-copy',
+      {
+        duration: 1,
+        marginTop: '-160px',
+      },
+      '<'
+    );
 })();
